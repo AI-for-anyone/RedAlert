8a59561d1dae870ac44bff79fdcde7eca4ff5178
AIRobot <airobot@airobot.link>
2025-08-19 17:29:28 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -172,7 +172,7 @@ class Config:
             "camera": ["camera", "move_camera", "视角"],
             "fight": ["attack", "occupy", "repair", "stop", "战斗", "army"],
             "info": ["get_game_state", "map_query", "find_path", "player_base", "screen_info", "visible", "explorer"],
-            "produce": ["produce", "can_produce", "query_production", "manage_production", "ensure_can_build", "生产", "deploy_mcv", "get_resource_info"],
+            "produce": ["produce", "can_produce", "query_production", "manage_production", "ensure_can_produce", "ensure_can_build", "生产", "deploy_mcv", "get_resource_info"],
             "unit": ["move_units", "query_actor", "select_units", "form_group", "deploy_units", "单位"]
         }
         
